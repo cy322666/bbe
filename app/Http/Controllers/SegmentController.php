df773f1bd6868d6499acbd01587d6cd9ca8474e7
cy322666 <cy322666@gmail.com>
2023-03-14 21:05:02 +0200

    add log

--- app/Http/Controllers/SegmentController.php
+++ app/Http/Controllers/SegmentController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Segment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SegmentController extends Controller
 {
     public function hook(Request $request)
     {
+        Log::info(__METHOD__, $request->toArray());
+
         $segment = Segment::query()->create([
             'lead_id' => $request->leads[0]['add']['id'],
         ]);
