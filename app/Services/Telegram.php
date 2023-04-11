66c15d19f6bb0127e3e28a82a97efb9794e37117
cy322666 <cy322666@gmail.com>
2023-04-11 14:04:48 +0200

    rafactor tg client

--- app/Services/Telegram.php
+++ app/Services/Telegram.php
@@ -2,19 +2,28 @@
 
 namespace App\Services;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
 class Telegram
 {
+    /**
+     * @throws GuzzleException
+     */
     public static function send(string $file, string $msg)
     {
-        $getQuery = [
-            "chat_id" 	 => '-979315059',
-            "text"  	 => "*Ошибка в коде!* \n*Где:* $file \n*Текст:* $msg",
-            "parse_mode" => "markdown"
-        ];
-        $ch = curl_init("https://api.telegram.org/bot". env('TG_TOKEN') ."/sendMessage?" . http_build_query($getQuery));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_exec($ch);
-        curl_close($ch);
+        if (strlen($msg) >= 4095) {
+
+            $msg = substr($msg, 0, 50);
+
+            (new Client())->get('https://api.telegram.org/bot' . env('TG_TOKEN') . '/sendMessage', [
+                'query' => [
+                    "chat_id" => '-979315059',
+                    "text" => "*Ошибка в коде!* \n*Где:* $file \n*Текст:* $msg",
+                    "parse_mode" => "markdown",
+//                'reply_markup' => json_encode(['inline_keyboard' => [[$keyboard]]]),
+                ]
+            ]);
+        }
     }
 }
