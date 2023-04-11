e926eb6e12ed306f3dde307b9382b59fd26c8a14
cy322666 <cy322666@gmail.com>
2023-04-11 13:40:49 +0200

    onec build

--- app/Jobs/OneCPay.php
+++ app/Jobs/OneCPay.php
@@ -2,22 +2,58 @@
 
 namespace App\Jobs;
 
+use App\Models\Account;
 use App\Models\OneC\Pay;
+use App\Services\amoCRM\Client;
+use App\Services\amoCRM\Models\Contacts;
+use App\Services\amoCRM\Models\Leads;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 class OneCPay implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function __construct(Pay $pay) {}
+    public function __construct(public Pay $pay) {}
 
     public function handle()
     {
-        //
+        try {
+            $amoApi = (new Client(Account::query()->first()))->init();
+
+            $amoApi->service->queries->setDelay(0.5);
+
+            $contact = Contacts::search(['Почта' => $this->pay->email], $amoApi);
+
+            $leads = $contact
+                ->leads
+                ->filter(function($lead) {
+
+                    return $lead->status_id == 142 && $lead->pipeline_id == 3342043;
+            });
+
+            $lead = $leads->count() > 0 ? $leads->first() : Leads::create($contact, [
+                'status_id'   => 142,
+                'pipeline_id' => 3342043,
+            ], 'Новая сделка из 1С');
+
+            $this->pay->lead_id = $lead->id;
+            $this->pay->contact_id = $contact->id;
+            $this->pay->save();
+
+            $result = Artisan::call('1c:pay-send '.$this->pay->id);
+
+            $this->pay->status = $result;
+            $this->pay->save();
+
+        } catch (\Throwable $e) {
+
+            Log::error(__METHOD__, [$e->getMessage().' '.$e->getFile().' '.$e->getLine()]);
+        }
     }
 }
