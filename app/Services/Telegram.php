d7745069769e87b207780785aef5f6d9b9c54783
cy322666 <cy322666@gmail.com>
2023-05-23 15:43:33 +0200

    hotfix markdown

--- app/Services/Telegram.php
+++ app/Services/Telegram.php
@@ -10,17 +10,22 @@ class Telegram
     /**
      * @throws GuzzleException
      */
-    public static function send(string $msg, string $chatId, string $token, ?array $keyboard)
+    public static function send(string $msg, string $chatId, string $token, ?array $keyboard, bool $isMarkdown = true)
     {
         if (strlen($msg) >= 4095) {
+
             $msg = substr($msg, 0, 50);
         }
 
         $body = [
             "chat_id" => $chatId,
             "text"    => $msg,
-            "parse_mode"   => "markdown",
         ];
+
+        if ($isMarkdown !== false) {
+
+            $body = array_merge($body, ["parse_mode" => "markdown"]);
+        }
 
         if (count($keyboard) > 0) {
 
