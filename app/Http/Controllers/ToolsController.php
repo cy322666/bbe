7f07876fcf7d9f1d1b58432623ea1d9517439658
cy322666 <cy322666@gmail.com>
2023-09-25 13:16:32 +0200

    fix add

--- app/Http/Controllers/ToolsController.php
+++ app/Http/Controllers/ToolsController.php
@@ -167,7 +167,9 @@ class ToolsController extends Controller
     {
         Log::info(__METHOD__, $request->toArray());
 
-        $leadId = $request->toArray()['leads']['add'][0]['id'] ?? $request->toArray()['status']['add'][0]['id'];
+        $leadId = $request->toArray()['leads']['add'][0]['id'] ?? null;// ?? $request->toArray()['status']['add'][0]['id'];
+
+        if (!$leadId) exit;
 
         $segment = Segment::query()->create([
             'lead_id' => $leadId,
