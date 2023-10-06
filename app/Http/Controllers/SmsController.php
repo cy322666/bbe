2c4f9d0310ac4a2f8566e6835b53271904b6949a
cy322666 <cy322666@gmail.com>
2023-10-06 15:54:03 +0200

    sms fix

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -36,7 +36,7 @@ class SmsController extends Controller
 
         $phone = $lead->cf('Телефон')->getValue();
 
-        $text = 'Вот ваш договор '.$lead->cf('Договор. Ссылка')->getValue().'. Вот код подтверждения: {код}. Подтвердите введя его тут '.$lead->cf('Договор. Анкета код')->getValue().'. Cайт https://bangbangeducation.ru';
+        $text = 'Ознакомиться с договором на обучение можно по ссылке '.$lead->cf('Договор. Ссылка')->getValue().'. Код подтверждения: {код}. Для подписания договора введите его тут '.$lead->cf('Договор. Анкета код')->getValue().'. Cайт https://bangbangeducation.ru';
 
         $result = $authCode->generateCode(
             $phone,
