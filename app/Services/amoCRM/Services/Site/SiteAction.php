d6b5674fefdbe4830b44869c2f83086708438511
cy322666 <cy322666@gmail.com>
2023-12-01 15:20:48 +0200

    add month credit

--- app/Services/amoCRM/Services/Site/SiteAction.php
+++ app/Services/amoCRM/Services/Site/SiteAction.php
@@ -72,14 +72,14 @@ class SiteAction
 
                 if (!empty($body->months))
                     $lead->cf('Рассрочка Месяцы')->setValue($body->months);
-
-                if (!empty($body->credit_price)) {
-
-                    $price = preg_replace("/[^0-9]/", '', $body->credit_price);
-
-                    $lead->cf('Стоимость в месяц')->setValue();
-                    $lead->sale = $price * $body->months;
-                }
+//
+//                if (!empty($body->credit_price)) {
+//
+//                    $price = preg_replace("/[^0-9]/", '', $body->credit_price);
+//
+//                    $lead->cf('Стоимость в месяц')->setValue();
+//                    $lead->sale = $price * $body->months;
+//                }
                 $lead->save();
 
                 $lead = LeadHelper::setUtmsForObject($lead, $body);
