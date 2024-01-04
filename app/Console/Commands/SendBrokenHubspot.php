8fac25ab7bbcfabd7ced579e722f1049b1a9352a
cy322666 <cy322666@gmail.com>
2024-01-04 15:14:28 +0200

    send broken hb

--- app/Console/Commands/SendBrokenHubspot.php
+++ app/Console/Commands/SendBrokenHubspot.php
@@ -50,6 +50,8 @@ class SendBrokenHubspot extends Command
 
         foreach ($brokens as $broken) {
 
+            $createLead = false;
+
             if (Broken::query()
                 ->where('id', '!=', $broken->id)
                 ->where('email', $broken->email)
@@ -59,10 +61,11 @@ class SendBrokenHubspot extends Command
                 $broken->is_double = true;
                 $broken->save();
 
+                break;
+
             } else {
 
                 try {
-
                     $course = $broken->courseid ?
                         Course::query()
                             ->where('course_id', $broken->courseid)
@@ -77,14 +80,12 @@ class SendBrokenHubspot extends Command
                     $productType = NoteHelper::getTypeProduct($broken);
 
                     if (!$contact) {
-
                         $contact = Contacts::create($amoApi, $broken->firstname);
 
                         $contact = Contacts::update($contact, [
                             'Почта' => $broken->email,
                             'Телефоны' => [$broken->phone],
                         ]);
-
                     } else {
                         //поиск в работе в основной
                         $leadActive = Leads::search($contact, $amoApi, [
@@ -105,70 +106,87 @@ class SendBrokenHubspot extends Command
                                     'Расскажите, пожалуйста, почему заинтересовались данным направлением?',
                                 ]);
 
-                            } catch (\Throwable $e) {
+                                $broken->lead_id = $leadActive->id;
+                                $broken->contact_id = $contact->id;
+                                $broken->status = 1;
+                                $broken->save();
 
+                            } catch (\Throwable $e) {
                                 Log::info(__METHOD__, [$e->getMessage(), $e->getTraceAsString()]);
                             }
-                        } else {
-//                            поиск успешной везде по дате создания, пока стоп
-                            $leadSuccess = Leads::searchSuccess($contact, $amoApi, [
-                                3342043,
-//                                6540894,
-//                                7206046,
-                            ]);
 
-                            if ($leadSuccess) {
-                                //менее трех дней как закрыт в успех?
-                                if(Carbon::parse($leadSuccess->closed_at)->addHours(3) <
-                                   Carbon::now()->addHours(3)->subDays(3)) {
-
-                                    //create
-                                    $lead = Leads::create($contact, [
-                                        'status_id' => $broken->is_test ? 53757562 : 33522700
-                                    ], 'Новый лид брошенная корзина');
-
-                                    $productType ? $lead->cf('Тип продукта')->setValue($productType) : null;
-                                    $productType ? $lead->attachTag($productType) : null;
-
-                                    try {
-                                        $lead->cf('Название продукта')->setValue(trim($broken->coursename));
-
-                                    } catch (\Throwable $e) {}
-
-                                    if ($course) {
-
-                                        $lead->sale = $course->price;
-                                        $lead->cf('Курсы (основное)')->setValue($course->name); //TODO
-                                    }
-
-                                    $lead->cf('ID курса')->setValue($broken->courseid);
-                                    $lead->cf('url')->setValue($broken->course_url);
-                                    $lead->cf('Источник')->setValue('Брошенные корзины');
-                                    $lead->save();
-
-                                    Notes::add($lead, [
-                                        'Клиент зашел на страницу курса и нажал "оплатить", но заявку не оставил',
-                                        'Очень важно связываться по телефону максимально оперативно',
-                                        'Заход в формате "интересовались обучением по курсу "название программы" предоставлю подробную программу и помогу разобраться',
-                                        'Расскажите, пожалуйста, почему заинтересовались данным направлением?',
-                                    ]);
-
-                                    Tags::add($lead, 'БрошеннаяКорзина');
-
-                                    $broken->lead_id = $lead->id;
-                                    $broken->contact_id = $contact->id;
-                                    $broken->status = 1;
-                                    $broken->save();
-                                }
-                            }
+                            break;
                         }
                     }
+
+                    // поиск успешной везде по дате создания, пока стоп
+                    $leadSuccess = Leads::searchSuccess($contact, $amoApi, [
+                        3342043,
+//                      6540894,
+//                      7206046,
+                    ]);
+
+                    if ($leadSuccess) {
+                        //менее трех дней как закрыт в успех?
+                        if (Carbon::parse($leadSuccess->closed_at)->addHours(3) <
+                            Carbon::now()->addHours(3)->subDays(3)) {
+
+                            $this->create($broken, $contact, $productType, $course);
+                        }
+                    } else
+                        $this->create($broken, $contact, $productType, $course);
+
                 } catch (\Throwable $e) {
-//                    dd($e->getMessage().' '.$e->getFile().' '.$e->getLine());
 
                     throw new \Exception($e->getMessage().' '.$e->getFile().' '.$e->getLine());
                 }
             }
+        }
+    }
+
+    public function create($broken, $contact, $productType, $course)
+    {
+        try {
+            $lead = Leads::create($contact, [
+                'status_id' => $broken->is_test ? 53757562 : 33522700
+            ], 'Новый лид брошенная корзина');
+
+            $productType ? $lead->cf('Тип продукта')->setValue($productType) : null;
+            $productType ? $lead->attachTag($productType) : null;
+
+            try {
+                $lead->cf('Название продукта')->setValue(trim($broken->coursename));
+
+            } catch (\Throwable $e) {}
+
+            if ($course) {
+
+                $lead->sale = $course->price;
+                $lead->cf('Курсы (основное)')->setValue($course->name); //TODO
+            }
+
+            $lead->cf('ID курса')->setValue($broken->courseid);
+            $lead->cf('url')->setValue($broken->course_url);
+            $lead->cf('Источник')->setValue('Брошенные корзины');
+            $lead->save();
+
+            Notes::add($lead, [
+                'Клиент зашел на страницу курса и нажал "оплатить", но заявку не оставил',
+                'Очень важно связываться по телефону максимально оперативно',
+                'Заход в формате "интересовались обучением по курсу "название программы" предоставлю подробную программу и помогу разобраться',
+                'Расскажите, пожалуйста, почему заинтересовались данным направлением?',
+            ]);
+
+            Tags::add($lead, 'БрошеннаяКорзина');
+
+            $broken->lead_id = $lead->id;
+            $broken->contact_id = $contact->id;
+            $broken->status = 1;
+            $broken->save();
+
+        } catch (\Throwable $e) {
+
+            throw new \Exception($e->getMessage().' '.$e->getFile().' '.$e->getLine());
         }
     }
 }
