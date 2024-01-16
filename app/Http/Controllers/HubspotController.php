95169def6b276cb39d7bb74d4a7fe3ff4617fc6a
cy322666 <cy322666@gmail.com>
2024-01-16 09:34:07 +0200

    hb add forms

--- app/Http/Controllers/HubspotController.php
+++ app/Http/Controllers/HubspotController.php
@@ -12,11 +12,11 @@ class HubspotController extends Controller
         $uuids = [
             '4c10b4fa-7fac-4c43-88e7-b074180b43dc',//
             '6925f713-ef2f-4d81-a0fa-7b605ac605d1',//
-            'eda7bd55-145f-4a27-92c3-e3b992397674',//
-            '24da75d3-e794-47b7-bb45-06c81d8a9597',//
-            'e56687c0-1141-49c7-a706-2fa2941322c8',//
-            '0af9faf3-c056-4023-8919-e654a384e21f',//
-            '77c0f622-3bb6-4412-b0b2-c46d424f7171',//
+            'eda7bd55-145f-4a27-92c3-e3b992397674',//+
+            '24da75d3-e794-47b7-bb45-06c81d8a9597',//+
+            'e56687c0-1141-49c7-a706-2fa2941322c8',//+
+            '0af9faf3-c056-4023-8919-e654a384e21f',//+
+            '77c0f622-3bb6-4412-b0b2-c46d424f7171',//+
 //            'b77eff0e-3661-4d48-b2d9-0757b35adb1d',
         ];
 
@@ -33,7 +33,7 @@ class HubspotController extends Controller
             '7f14eef7-c46b-4051-ad1d-557ce3d280e4',//
 //            '09658544-d246-47f3-bea2-3d9f0bcb266d',
 //            '7938618a-f89c-49db-83f4-18823db33259',
-            '5a21b267-6531-44c3-a476-7cd4aae9260f',//
+            '5a21b267-6531-44c3-a476-7cd4aae9260f',//+
 //            'b170c128-18fd-4157-a714-77977f377b70',
 //            '2821cfc4-d327-4ea4-b3b0-accedbbbc551',
             '9be94507-6b84-441f-9648-9238fe26ff93',//
@@ -74,6 +74,11 @@ class HubspotController extends Controller
         $uuids = [
             '5a21b267-6531-44c3-a476-7cd4aae9260f',//+
             '77c0f622-3bb6-4412-b0b2-c46d424f7171',//+
+            'eda7bd55-145f-4a27-92c3-e3b992397674',//+
+            '24da75d3-e794-47b7-bb45-06c81d8a9597',//+
+            'e56687c0-1141-49c7-a706-2fa2941322c8',//+
+            '0af9faf3-c056-4023-8919-e654a384e21f',//+
+
         ];
 
         foreach ($uuids as $uuid) {
